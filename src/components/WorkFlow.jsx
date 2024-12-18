2451180859d5c9d86e77e44e197f10777edf2375
Fezao187 <katlegomaredi9@gmail.com>
2024-12-18 07:43:38 +0200

    Testimonials section

--- src/components/WorkFlow.jsx
+++ src/components/WorkFlow.jsx
@@ -10,7 +10,7 @@ const WorkFlow = () => {
         text-center mt-6 tracking-wide'>Accelerate your
                 <span className='bg-gradient-to-r from-orange-500 
                 to-orange-800 text-transparent bg-clip-text'>
-                    {' '}coding workflow.
+                    {' '}coding workflow
                 </span>
             </h2>
             <div className="flex flex-wrap justify-center">
