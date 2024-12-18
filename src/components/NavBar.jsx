ec9c81f6b3fda2eb4794b121b672afc90877d2d9
Fezao187 <katlegomaredi9@gmail.com>
2024-12-18 03:03:47 +0200

    NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -43,11 +43,19 @@ const NavBar = () => {
                 flex flex-col justify-center items-center lg:hidden">
                     <ul>
                         {navItems.map((item, index)=>(
-                            <li key={index}>
+                            <li key={index} className="py-4">
                                 <a href={item.href}>{item.label}</a>
                             </li>
                         ))}
                     </ul>
+                    <div className="flex space-x-6">
+                        <a href="#" className="py-2 px-3 border rounded-md">
+                            Sign In
+                        </a>
+                        <a href="#" className="py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800">
+                            Create an account
+                        </a>
+                    </div>
                 </div>
             )}
         </div>
